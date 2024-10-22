ff356d0044b5475062989fd84245ea5b606b4992
Avieon Greene <Avieongreene16@yahoo.com>
2024-10-22 19:44:43 -0400

    added  form validations to javascript and html.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
- 
+ const phoneNumber = document.getElementById("number");
+ const fname = document.getElementById("name");
+ const email = document.getElementById("email");
+
+
+ function emailValidation() {
+    if (email.value.length >= 10) {
+        email.style.border = "3px solid #89fc00";
+       }else {
+        email.style.border = "3px solid red";
+       };
+ }
+
+function fullNameValidation () {
+    if (fname.value.length >= 4) {
+        fname.style.border = "3px solid #89fc00";
+       }else {
+        fname.style.border = "3px solid red";
+       };
+}
+
+
+ function numberValidation() {
+   
+    if ( phoneNumber.value.length === 10 ) {
+         phoneNumber.style.border = "3px solid #89fc00";
+    }else {
+        phoneNumber.style.border = "3px solid red";
+    };
+
+ }
+
+ email.addEventListener("input", emailValidation);
+ fname.addEventListener("input", fullNameValidation);
+ phoneNumber.addEventListener("input", numberValidation);
